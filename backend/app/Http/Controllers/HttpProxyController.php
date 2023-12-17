331270ba6604a946fd5f637c37553f0e4a1aa31e
uwla <uwlalinux@gmail.com>
2023-12-17 15:30:31 -0300

    make proxy requests via dedicated controller

---
+++ backend/app/Http/Controllers/HttpProxyController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Request;
+use GuzzleHttp\Client as HttpCLient;
+
+class HttpProxyController extends Controller
+{
+    // proxy current request to the FRONTEND
+    public function proxy()
+    {
+        $base_uri = env('FRONTEND_URI', 'http://localhost');
+
+        // create http client
+        $client = new HttpCLient([
+            'base_uri' => $base_uri,
+            'http_errors' => false, // disable guzzle exception on 4xx or 5xx response code
+            'timeout' => 20.0,
+        ]);
+
+        // extract request information to be passed to the proxy
+        $path = Request::path();
+        $method = RequesT::method();
+        $headers = $this->filter_headers(Request::header());
+        $query = Request::getQueryString();
+        $body = Request::getContent();
+
+        // perform a request acting as a proxy
+        $response = $client->request($method, $path, [
+            'headers' => $headers,
+            'query' => $query,
+            'body' => $body,
+        ]);
+
+        // extract information
+        $content = $response->getBody()->getContents();
+        $status_code = $response->getStatusCode();
+        $headers = $this->filter_headers($response->getHeaders());
+
+        // return the request
+        return response($content, $status_code)->withHeaders($headers);
+    }
+
+    // simple helper function to filter header array on request & response
+    public function filter_headers($headers)
+    {
+        // currently, we will only allow these two headers to be passed for safety reasons
+        $allowed_headers = ['accept', 'content-type'];
+
+        $new_headers = [];
+        foreach ($headers as $key => $value)
+            if (in_array(strtolower($key), $allowed_headers))
+                $new_headers[$key] = $value;
+        return $new_headers;
+    }
+}
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HttpProxyController;
 use App\Http\Middleware\EnsurePaidSats;
 use Illuminate\Support\Facades\Route;
 use GuzzleHttp\Client as HttpClient;
@@ -22,50 +23,6 @@ Route::post('/lnpay/pay', [LNPaymentController::class, 'pay']);
 Route::post('/lnpay/confirm', [LNPaymentController::class, 'confirmPayment']);
 Route::post('/lnpay/confirmJSON', [LNPaymentController::class, 'confirmPaymentJSON']);
 
-// simple helper function to filter header array on request & response
-function filter_headers($headers)
-{
-    // currently, we will only allow these two headers to be passed for safety reasons
-    $allowed_headers = ['accept', 'content-type'];
-
-    $new_headers = [];
-    foreach ($headers as $key => $value)
-        if (in_array(strtolower($key), $allowed_headers))
-            $new_headers[$key] = $value;
-
-    return $new_headers;
-}
-
-Route::middleware(EnsurePaidSats::class)->any('/{path}', function() {
-    $base_uri = env('FRONTEND_URI', 'http://localhost');
-
-    // create http client
-    $client = new GuzzleHttp\Client([
-        'base_uri'    => $base_uri,
-        'http_errors' => false, // disable guzzle exception on 4xx or 5xx response code
-        'timeout'     => 20.0,
-    ]);
-
-    // extract request information to be passed to the proxy
-    $path = Request::path();
-    $method = RequesT::method();
-    $headers = filter_headers(Request::header());
-    $query = Request::getQueryString();
-    $body = Request::getContent();
-
-    // perform a request acting as a proxy
-    $response = $client->request($method, $path, [
-        'headers' => $headers,
-        'query'   => $query,
-        'body'    => $body,
-    ]);
-
-    // extract information
-    $content = $response->getBody()->getContents();
-    $status_code = $response->getStatusCode();
-    $headers = filter_headers($response->getHeaders());
-
-    // return the request
-    return response($content, $status_code)->withHeaders($headers);
-
-})->where('path', '.*'); // required to allow $path to catch all sub-path
+Route::middleware(EnsurePaidSats::class)
+    ->any('/{path}', [HttpProxyController::class, 'proxy'])
+    ->where('path', '.*');
